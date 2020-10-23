d3aabe81a45a4f509894a643b3d0c83bdf31b8e6
Lidia Alonso <l.alonsodiaz@gmail.com>
2020-10-23 14:06:54 +0200

    changes to css and model

---
+++ js/modelo.js
@@ -0,0 +1,31 @@
+//modelo de tareas
+var listaTareas= new Array();
+
+listaTareas= [
+
+       {'idTarea':0,
+         'titulo': 'Estudiar Javascript',
+         'prioridad':'urgente',
+    
+        },
+        {'idTarea':2,
+        'titulo': 'Hacer yoga',
+        'prioridad':'Diaria',
+       },
+       {'idTarea':3,
+       'titulo': 'Ir al cine',
+       'prioridad':'Mensual',
+      },
+
+      {'idTarea':4,
+      'titulo': 'estudiar inglés',
+      'prioridad':'Diaria'
+     },
+
+
+     {'idTarea':5,
+     'titulo': 'pagar el alquiler',
+     'prioridad':'urgente'
+    },
+    
+];
